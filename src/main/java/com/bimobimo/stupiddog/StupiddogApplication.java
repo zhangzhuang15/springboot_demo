59992f81bd07f8b9da692c06a6614d8d78d724a2
zhangzhuang <2592004894@qq.com>
2021-01-13 22:43:13 +0800

    init springboot demo

---
+++ src/main/java/com/bimobimo/stupiddog/StupiddogApplication.java
@@ -0,0 +1,13 @@
+package com.bimobimo.stupiddog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StupiddogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StupiddogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bimobimo/stupiddog/StupiddogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bimobimo.stupiddog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StupiddogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
